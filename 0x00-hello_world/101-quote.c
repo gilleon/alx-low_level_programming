974f71c9c582a5c9368121bb7e1b4af85ee5bded
Gilbert <elormnizi@gmail.com>
2023-02-16 02:39:08 -0800

    mehn

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,8 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" \
-			- Dora Korpar, 2015-10-19\n");
-
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      s);
+	      59);
 	return (1);
 }
