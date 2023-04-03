41b8e377db318ebf20475bd3439f191bd02691bc
gilleon <elormnizi@gmail.com>
2023-04-03 11:22:38 -0700

    add list_len function

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: linked list head
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t counter;
+
+	for (counter = 0; h != NULL; counter++, h = h->next);
+
+	return (counter);
+}
