2063848ce1fd60715f6690289c21d1c42a99eadf
gilbert <elormnizi@gmail.com>
2023-06-19 03:57:20 -0700

    correction to task 1

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -1,21 +1,7 @@
+#include <unistd.h>
 #include "main.h"
 
-/**
- * main - Prints "_putchar" followed by a new line.
- *
- * Return: Always 0.
- */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	write(1, &c, 1);
 }
